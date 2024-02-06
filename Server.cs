9da3030d454f7e22b4760229e3fb4f2e812fa37b
Jennie Galmin <Jennie.galmin@gmail.com>
2024-02-06 11:53:19 +0100

    skrev om till meddelanden istället för webb address

--- Server.cs
+++ Server.cs
@@ -3,9 +3,14 @@ using System.Net;
 using System.Net.Sockets;
 using MongoDB.Driver;
 
+
 public class Server{
     private static List<Socket> clients = new List<Socket>();
-    public static void StartServer(){
+    public static void StartServer(IMongoCollection<User> collection){
+
+    var userCollection = collection; 
+    Login login = new Login(userCollection);
+
     IPAddress ipAddress = new IPAddress(new byte[] {127, 0, 0, 1});
     IPEndPoint ipEndpoint = new IPEndPoint(ipAddress, 25500);
 
@@ -18,33 +23,29 @@ public class Server{
     serverSocket.Bind(ipEndpoint);
     serverSocket.Listen();
     // så att den vet vilken ip address den ska lyssna till
+
     while(true){ //while loop för att kunna acceptera alla klienter
         Socket client = serverSocket.Accept();
         //för att kunna acceptera klienterna
         clients.Add(client);
+        // för att lägga till kienterna
        
-        foreach(Socket c in clients){
-            if( c != client){
-                byte[] loginMessage = System.Text.Encoding.UTF8.GetBytes($"{username} har loggat in");
-                c.Send(loginMessage);
-            }
-        // en loop för att gå igenom listan av klienter, om c inte är client kommer en meddelande ruta dyka upp 
-        // för som andra som är inloggade
-        // Behöver få in uername ist för client
-        }
-
         byte [] incoming = new byte [5000];
         int read = client.Receive(incoming);
-        string request = System.Text.Encoding.UTF8.GetString(incoming, 0, read);
+        string message = System.Text.Encoding.UTF8.GetString(incoming, 0, read);
+        // gör så att bytes som kommer in från client blir till string format
 
-        Console.WriteLine($"Request: {request}");
-
-        string html = "<html><head></head><body><h1>Welcome to chat</h1></body></html>";
-        string response = $"HTTP/1.1 200 OK\nConnection: close\nContent-type: text/html\n\n{html}";
-
-        byte[] buffer = System.Text.Encoding.UTF8.GetBytes(response);
-        client.Send(buffer);
-        client.Close();
+        if( message != null){
+            Console.WriteLine($"Request: {message}");
+            string response = $"{login.GetUsername()} ";
+                    // svaret i en string
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(response);   
+            client.Send(buffer);  
+            client.Close();   
+        }
+        
+        }
+       
     }
-}
+
 }
